9ca1409ff770a68cc5afcae2eeadc78ddbc85a6f
kroleg <kr.oleg@gmail.com>
2021-02-18 02:29:23 +0300

    Task one - time interval

---
+++ task_one_time_interval.py
@@ -0,0 +1,25 @@
+duration = 400153
+
+seconds = duration % 60
+
+duration = duration - seconds
+
+days = int(duration / (24 * 60 * 60))
+
+duration = duration - (days * 24 * 60 * 60)
+
+hours = int(duration / (60 * 60))
+
+duration = duration - (hours * 60 * 60)
+
+minutes = int(duration / 60)
+
+date = [[days, "дн"], [hours, "час"], [minutes, "мин"], [seconds, "сек"]]
+
+summary = str()
+
+for d in date:
+    if d[0] > 0:
+        summary += str(d[0]) + " " + d[1] + " "
+
+print(summary)
