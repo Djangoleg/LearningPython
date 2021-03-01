64bdf97d7100394c170d89de6dc7c008cc4e11c4
kroleg <kr.oleg@gmail.com>
2021-03-01 23:36:23 +0300

    Bug fix task_five

--- task_five_jokes.py
+++ task_five_jokes.py
@@ -10,6 +10,7 @@ def get_jokes(number_jokes, is_unique):
     """Функция возвращает заданное количество number_jokes шуточных фраз.
        Можно использовать ключ уникальности is_unique, чтобы слова не повторялись
        во фразах."""
+
     jokes_list = list()
 
     if is_unique:
@@ -18,13 +19,19 @@ def get_jokes(number_jokes, is_unique):
         shuffle(adjectives)
         nouns_list = nouns[:number_jokes]
         adverbs_list = adverbs[:number_jokes]
-        adverbs_adjectives = adjectives[:number_jokes]
+        adjectives_list = adjectives[:number_jokes]
+
+        # Проверка на max index
+        length_list = [len(nouns_list), len(adverbs_list), len(adjectives_list)]
+        max_idx = min(length_list)
+        if number_jokes > max_idx:
+            number_jokes = max_idx
 
         for x in range(number_jokes):
             word_list = list()
             word_list.append(nouns_list[x])
             word_list.append(adverbs_list[x])
-            word_list.append(adverbs_adjectives[x])
+            word_list.append(adjectives_list[x])
             jokes_list.append(" ".join(word_list))
 
     else:
@@ -38,5 +45,5 @@ def get_jokes(number_jokes, is_unique):
     return jokes_list
 
 
-print(get_jokes(4, True))
-print(get_jokes(4, False))
+print(get_jokes(55, True))
+print(get_jokes(44, False))
