3dca63a65d32c82f8ee3542ad54d580e36069879
kroleg <kr.oleg@gmail.com>
2021-03-04 20:56:30 +0300

    Improvements

--- task_four_utils.py
+++ task_four_utils.py
@@ -2,6 +2,8 @@ from datetime import datetime
 from decimal import Decimal
 from requests import get, utils
 
+val_server_url = "http://www.cbr.ru/scripts/XML_daily.asp"
+
 
 def currency_rates(argv):
     program, *args = argv
@@ -9,16 +11,15 @@ def currency_rates(argv):
     summary_list = []
 
     # Get server data.
-    response = get("http://www.cbr.ru/scripts/XML_daily.asp")
+    response = get(val_server_url)
     encodings = utils.get_encoding_from_headers(response.headers)
     content = str(response.content.decode(encoding=encodings))
 
     # Hard coding.
-    tmp_list = content.split('<Valute')
-    start_idx = tmp_list[0].index("Date=") + len("Date=") + 1
-    end_idx = tmp_list[0][start_idx:].index('"')
-    val_dict["date"] = datetime.strptime(tmp_list[0][start_idx:][:end_idx], '%m.%d.%Y')
-    tmp_list = tmp_list[1::]
+    date_str = content.split('<ValCurs')[1].split('name="Foreign Currency Market">')[0].split('Date="')[1].split('"')[0]
+    val_dict["date"] = datetime.strptime(date_str, '%m.%d.%Y')
+
+    tmp_list = content.split('<Valute')[1::]
 
     for row in tmp_list:
         key, value = row.split('<CharCode>')[1].split('</CharCode>')[0], row.split('<Value>')[1].split('</Value>')[0]
@@ -37,4 +38,5 @@ def currency_rates(argv):
 
 if __name__ == '__main__':
     import sys
+
     print(currency_rates(sys.argv))
