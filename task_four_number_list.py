e4893592508b78fa329fe902c5629e009aff982a
kroleg <kr.oleg@gmail.com>
2021-03-07 19:46:32 +0300

    Task four. Short entry

--- task_four_number_list.py
+++ task_four_number_list.py
@@ -1,9 +1,5 @@
 src = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
 
-result_lst = list()
-for ind, value in enumerate(src):
-    if ind > 0:
-        if value > src[ind - 1]:
-            result_lst.append(value)
+result_lst = [value for ind, value in enumerate(src) if ind > 0 and value > src[ind - 1]]
 
 print(result_lst)
