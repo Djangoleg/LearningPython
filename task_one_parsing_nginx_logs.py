6e64c9509aa179577805aa74fc69e4720c3f20c9
kroleg <kr.oleg@gmail.com>
2021-03-10 23:55:02 +0300

    Task one. Spammers optimization

--- task_one_parsing_nginx_logs.py
+++ task_one_parsing_nginx_logs.py
@@ -8,14 +8,17 @@ with open("nginx_logs.txt", 'r', encoding='utf-8') as nginx_logs:
         requested_resource = method_type_and_request_row[1]
         final_list.append((remote_addr, request_type, requested_resource))
 
-# print(*final_list, sep="\n")
 # Поиск спамера.
-spamers = dict()
+spammers = dict()
 for i in final_list:
-  spamers[i[0]] = spamers.get(i[0], 0) + 1
+    spammers[i[0]] = spammers.get(i[0], 0) + 1
 
-# Сортировка по максимальному количеству коннектов. Order by Descending for Value.
-print(dict(sorted(spamers.items(), key=lambda item: item[1], reverse=True)))
+max_spammer_key = max(spammers, key=spammers.get)
+print(f"Max number of connections from IP: {max_spammer_key}. "
+      f"Number of connections: {spammers[max_spammer_key]}")
 
-
-
+print("*" * 20)
+# Дополнительная проверка. Выведем тех, кто подключался больше 1000 раз.
+for key in sorted(spammers, key=spammers.get, reverse=True):
+    if spammers[key] > 1000:
+        print(key, spammers[key])
