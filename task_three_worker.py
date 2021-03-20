c3cb8c7cdf9f8e59dbc909b65b6def59859e76b8
kroleg <kr.oleg@gmail.com>
2021-03-21 02:54:43 +0300

    Task three

---
+++ task_three_worker.py
@@ -0,0 +1,29 @@
+class Worker:
+    name = str()
+    surname = str()
+    position = str()
+    _income = dict()
+
+    def __init__(self, name, surname, position, wage, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {"wage": wage, "bonus": bonus}
+
+
+class Position(Worker):
+
+    def __init__(self, name, surname, position, wage, bonus):
+        super().__init__(name, surname, position, wage, bonus)
+
+    def get_full_name(self):
+        return f"{self.name} {self.surname}, {self.position}"
+
+    def get_total_income(self):
+        return round(sum(self._income.values()), 2)
+
+
+p = Position("Иван", "Иванов", "Директор", 200.56, 100.53)
+print(p.get_full_name())
+print(p.get_total_income())
+
