da91575f75caf1510fb197abfc9d5735985f2ec6
kroleg <kr.oleg@gmail.com>
2021-03-30 09:48:49 +0300

    Task three. Bug fix

--- task_three_except_number.py
+++ task_three_except_number.py
@@ -3,14 +3,33 @@ class NotNumber(Exception):
         self.txt = txt
 
 
+def try_parse_int(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+
+def try_parse_float(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+
 inp_data = input("Введите число: ")
 print(inp_data, sep="\n")
 num_list = list()
 
 while inp_data.lower() != "stop" and inp_data.lower() != "exit":
     try:
-        if inp_data.isdigit():
+        if try_parse_int(inp_data):
             num_list.append(int(inp_data))
+
+        elif try_parse_float(inp_data):
+            num_list.append(float(inp_data))
         else:
             raise NotNumber(f"\nВы ввели - '{inp_data}'. Это не число!")
 
