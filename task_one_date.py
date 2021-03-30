c589dc9d4e062883222fd8dbb6d684da4b0e2fc6
kroleg <kr.oleg@gmail.com>
2021-03-30 09:17:49 +0300

    Task one. bugfix

--- task_one_date.py
+++ task_one_date.py
@@ -7,25 +7,33 @@ class Date:
 
     @classmethod
     def get_number_for_date(cls):
-        date_time = datetime.datetime.strptime(cls.date, "%d.%m.%Y")
-        return [date_time.day, date_time.month, date_time.year]
+        try:
+            date_time = datetime.datetime.strptime(cls.date, "%d.%m.%Y")
+        except ValueError as e:
+            print(f"ValueError: {e}")
+        else:
+            return [date_time.day, date_time.month, date_time.year]
 
     @staticmethod
     def check_date(day, month, year):
         if not 1 <= day <= 31:
-            print(f"Номер дня '{day}' должн быть в интервале от 1..31")
+            print(f"Номер дня '{day}' должен быть в интервале от 1..31")
             return False
         if not 1 <= month <= 12:
-            print(f"Номер месяца '{year}' должн быть в интервале от 1..12")
+            print(f"Номер месяца '{month}' должен быть в интервале от 1..12")
             return False
-        if not year:
-            print(f"Год должен быть инициализирован")
+        if not 1 <= year <= 9999:
+            print(f"Год '{year}' должен быть в интервале от 1..9999")
             return False
 
         return True
 
 
 date = Date("27.03.2021")
-print(date.get_number_for_date())
-print(date.check_date(*date.get_number_for_date()))
+dmy = date.get_number_for_date()
+if dmy:
+    print(dmy)
+    print(date.check_date(*dmy))
+
+print(date.check_date(27, 3, 10000))
 
